fad21c6edea986d6fe2e1aa0f03f8d2a9f181011
yashpatil1907 <patilyash1907@gmail.com>
2026-08-02 11:08:39 +0530

    Add ECS Cluster

--- TF-file/service.tf
+++ TF-file/service.tf
@@ -1,25 +1,18 @@
 resource "aws_ecs_service" "demo" {
-
-
-  name = "demo-service"
-
-
-  cluster = aws_ecs_cluster.demo.id
-
-
+  name            = "demo-service"
+  cluster         = aws_ecs_cluster.demo.id
   task_definition = aws_ecs_task_definition.demo.arn
 
-
   desired_count = 1
-
-
-  launch_type = "EC2"
-
+  launch_type   = "EC2"
 
   deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
 
+  scheduling_strategy = "REPLICA"
 
-  deployment_maximum_percent = 200
-
-
+  depends_on = [
+    aws_ecs_cluster.demo,
+    aws_ecs_task_definition.demo
+  ]
 }
